459e9de0b2763682cd2fb6dcae2d6d1ec7b6a627
windchange7 <windchange7@gmail.com>
2019-10-14 23:35:43 +0300

    threads started

---
+++ ConsoleAppTest/ProgramFlow/Threads.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleAppTest.ProgramFlow
+{
+    // Threads	are	a	lower	level	of	abstraction	than	tasks.	A	Task	object	represents	an item	of	work	to	be	performed,	
+    // whereas	a	Thread	object	represents	a	process running	within	the	operating	system. 
+
+    // Threads	are	created	as	foreground	processes	(although	they	can	be	set	to	run in	the	background).	
+    // The	operating	system	will	run	a	foreground	process	to completion,	which	means	that	an	application	will	not	terminate	
+    // while	it contains an  active foreground  thread.A foreground  process that    contains an infinite loop    will execute forever,	
+    // or until   it throws  an uncaught    exception or    the operating   system terminates  it.Tasks are created	as	background processes. 
+    // This    means that    tasks can be terminated  before they    complete    if all the foreground threads in an application complete. 
+    // Threads have    a priority    property that    can be  changed during  the lifetime    of the  thread. 
+    // It  is	not possible    to set the priority    of a   task.This gives   a thread  a higher    priority request so a   greater portion of available   
+    // processor time	is allocated.A thread  cannot deliver a result  to another thread.Threads must communicate by  using	shared	variables,	
+    // which can introduce synchronization   issues.It	is	not possible    to create  a continuation    on a   thread.
+    // Instead,	threads provide a method  called a   join,	which allows  one thread  to pause   until another   completes.
+    // It	is	not possible    to aggregate   exceptions over    a number  of threads.An exception thrown  inside a   thread must   
+    // be caught  and dealt   with by  the code in that thread.	Tasks provide exception aggregation, but threads don’t.
+    public class Threads
+    {
+        private void ThreadHello()
+        {
+            Console.WriteLine("Hello	from	the	thread");
+            Thread.Sleep(2000);
+        }
+
+        // The	Thread	class	is	located	in	the	System.Threading	namespace.	
+        // When you	create	a	Thread	you	can	pass	the	constructor	the	name	of	the	method	the thread	will	run.	
+        // Once	the	thread	has	been	created,	you	can	call	the	Start method	on	the	thread	to	start	it	running.	
+        public void CreateThreads()
+        {
+            Thread thread = new Thread(ThreadHello);
+            thread.Start();
+        }
+
+        // Note	that earlier versions of .NET required the creation	of a ThreadStart delegate to specify the method to be executed by the thread.	
+        // The	ThreadStart	delegate	is	no	longer	required. 
+        public void CreateThreadStart()
+        {
+            ThreadStart ts = new ThreadStart(ThreadHello);
+            Thread thread = new Thread(ts);
+            thread.Start();
+        }
+
+        // It	is	possible	to	start	a	thread	using	a	lambda	expression	to	specify	the	action	of the	thread
+        // 
+        public void CreateThreadWithLambda()
+        {
+            Thread thread = new Thread(() => 
+            {
+                Console.WriteLine("Hello	from	the	thread");
+                Thread.Sleep(1000);
+            });
+            thread.Start();
+            Console.WriteLine("Press	a	key	to	end.");
+        }
+    }
+}
--- ConsoleAppTest/Types/CreateTypes.cs
+++ ConsoleAppTest/Types/CreateTypes.cs
@@ -35,6 +35,108 @@ namespace ConsoleAppTest.Types
             }
         }
 
+        public enum AlienState
+        {
+            Sleeping,
+            Attacking,
+            Destroyed
+        }
+
+        public enum AlienStateByte : Byte
+        {
+            Sleeping = 1,
+            Attacking = 2,
+            Destroyed = 4
+        }
+
+        class AlienClass
+        {
+            public int X; public int Y; public int Lives;
+            public AlienClass(int x, int y) { X = x; Y = y; Lives = 3; }
+            public override string ToString() { return string.Format("X:	{0}	Y:	{1}	Lives:	{2}", X, Y, Lives); }
+        }
+
+        class MyStack<T> where T : class 
+            // If you want to restrict it to only store reference types you can add constraint on possible types that T can represent.
+        {
+            int stackTop = 0;
+            T[] items = new T[100];
+
+            public void Push(T item)
+            {
+                if (stackTop == items.Length) throw new Exception("Stack	full");
+                items[stackTop] = item; stackTop++;
+            }
+
+            public T Pop()
+            {
+                if (stackTop == 0) throw new Exception("Stack	empty");
+                stackTop--;
+                return items[stackTop];
+            }
+
+            // where	T	: new() ->   The type    T must    have a   public,	parameterless,	constructor
+            // where	T	: <base	class> ->  The type    T must    be of  type	base	class or  derive from	base	class.
+            // where   T	: <interface name> -> The type    T must    be or  implement the specified interface.	You can specify multiple    interfaces.
+            // where T	: unmanaged ->  The type T   must not be a   reference type    or contain any members which are reference types.
+        }
+
+        class AlienClassConstr
+        {
+            public int X;
+            public int Y;
+            public int Lives;
+
+            public AlienClassConstr() : this(1, 1, 3)
+            {
+                // A program can avoid   code repetition  by making  one constructor call another constructor by  use of  the keyword this.
+                // 	It	forms	a	call	of	another constructor	in	the	object.	In	the	program	below	the	parameters	to	the	call	
+                // of	one constructor	are	passed	into	a	call	of	another,	along	with	an	additional	lives	value. 
+                // Note	that	this	means	the	actual	body	of	the	constructor	is	empty,	because	all	of the	work	is	performed	
+                // by	the	call	to	the	other	constructor.	Another	way	to provide	default	values	to	a	constructor	is	to	make	
+                // use	of	optional	parameters.
+                // When	creating	objects	that	are	part	of	a	class	hierarchy,	a	programmer	must ensure	that	
+                // information	required	by	the	constructor	of	a	parent	object	is	passed into	a	parent	constructor.
+            }
+
+            public AlienClassConstr(int x, int y)
+            {
+                if (x < 0 || y < 0)
+                    throw new Exception("Invalid coordinates");
+                X = x;
+                Y = y;
+                Lives = 3;
+            }
+
+            public AlienClassConstr(int x, int y, int lives)
+            {
+                X = x;
+                Y = y;
+                Lives = lives;
+            }
+
+            public override string ToString() { return string.Format("X:	{0}	Y:	{1}	Lives:	{2}", X, Y, Lives); }
+        }
+
+        // A class can contain a static constructor method. This is called once before the creation of the very first instance of the class.	
+        class AlienStaticConstructor
+        {
+            static AlienStaticConstructor()
+            {
+                // When	the program	runs,	the	message	is	printed	once,	before	the	first	alien	is	created.	
+                // The static	constructor	is	not	called	when	the	second	alien	is	created.
+                Console.WriteLine("Static	Alien	constructor	running");
+            }
+            // A static constructor is a good place to load resources and initialize values  will be used by instances of the class.	
+            // This	can	include	the	values	of	static members	of	the	class,	as	described	in	the	next	section. 
+        }
+
+
+
+        /// <summary>
+        /// /////////////////
+        /// </summary>
+
 
         // The diff between reference and value types is crucial
         public void ValueAndReferenceTypes()
@@ -85,6 +187,76 @@ namespace ConsoleAppTest.Types
             Alien[] swarm = new Alien[100];
             Console.WriteLine("swarm	[0]	{0}", swarm[0].ToString());
             Console.ReadKey();
+        }
+
+        // Enumerated types are used in situations where the programmer wants to specify a range of values that a given type can have.	
+        // Unless	specified	otherwise,	an	enumerated	type	is	based	on	the	int	type	and the	enumerated	values	are	numbered	
+        // starting	at	0.	You	can	modify	this	by	adding extra	information	to	the	declaration	of	the	enum. 
+        public void CreateEnum()
+        {
+            AlienState a = AlienState.Sleeping;
+            Console.WriteLine(a); // prints	the	ToString result returned by the	AlienState variable, which	will output	the	string "Sleeping"
+            // Also can use casting to int to have numeric value printed
+        }
+
+        // The	basis	of	C#	reference	types	is	the	C#	class.	A	class	is	declared	in	a	very similar	
+        // manner	to	a	structure,	but	the	way	that	classes	are	manipulated	in	a program	is	significantly	different.	
+        // The	use	of	the	new	keyword to	create	a	new	object	is	said	to	create	a	new	instance	of	a	class.
+        public void CreateReference()
+        {
+            AlienClass x = new AlienClass(100, 100);
+            Console.WriteLine("x	{0}", x);
+            AlienClass[] swarm = new AlienClass[100];
+            for (int i = 0; i < swarm.Length; i++)
+                swarm[i] = new AlienClass(0, 0);
+
+            Console.WriteLine("swarm	[0]	{0}", swarm[0]);
+        }
+
+        // Memory	to	be	used	to	store	variables	of	value	type	is	allocated	on	the	stack.	The stack	
+        // is	an	area	of	memory	that	is	allocated	and	removed	as	programs	enter	and leave	blocks.	
+        // Any	value	type	variables	created	during	the	execution	of	a	block are	stored	on	a	local	
+        // stack	frame	and	then	the	entire	frame	is	discarded	when	the block	completes.	
+        // This	is	an	extremely	efficient	way	to	manage	memory. Memory	to	be	used	to	store	variables	of	reference	type	
+        // is	allocated	on	a different	structure,	called	the	heap.	The	heap	is	managed	for	an	entire application.	
+        // The	heap	is	required	because,	as	references	may	be	passed	between method	calls	as	parameters,	
+        // it	is	not	the	case	that	objects	managed	by	reference can	be	discarded	when	a	method	exits.	
+        // Objects	can	only	be	removed	from	the heap	when	the	garbage	collection	process	determines	that	
+        // there	are	no	references to	them.
+
+        // Generic	types	are	used	extensively	in	C#	collections,	such	as	with	the	List	and Dictionary	classes.	
+        // They	allow	you	to	create	a	List	of	any	type	of	data,	or a	Dictionary	of	any	type,	indexed	on	any	type.
+        // Without	generic	types	you	either	have	to	reduce	the	type	safety	in	your programs	by	using	collections	that	
+        // manage	only	objects,	or	you	have	to	waste	a lot	of	time	creating	different	collection	classes	for	each	
+        // different	type	of	data	that you	want	to	store. 
+        // 
+        public void UsingGenericTypes()
+        {
+            MyStack<string> nameStack = new MyStack<string>();
+            nameStack.Push("Rob");
+            nameStack.Push("Mary");
+            Console.WriteLine(nameStack.Pop());
+            Console.WriteLine(nameStack.Pop());
+            Console.ReadKey();
+        }
+
+        // A	constructor	allows	a	programmer	to	control	the	process	by	which	objects	are created.	
+        // Constructors	can	be	used	with	value	types	(structures)	and	reference types	(classes).	
+        // A	constructor	has	the	same	name	as	the	object	it	is	part	of	but does	not	have	a	return	type. 
+        // Constructors	can	perform	validation	of	their	parameters	to	ensure	that	any objects	that	are	created	contain	valid	information.	
+        // If	the	validation	fails,	the constructor	must	throw	an	exception	to	prevent	the	creation	of	an	invalid	object.
+        public void CreateConstructors()
+        {
+            AlienClassConstr x = new AlienClassConstr(100, 100);
+            Console.WriteLine("x	{0}", x);
+            // Constructors	can	be	given	access	to	modifiers.	If	an	object	only	has	a	private	constructor	it	cannot	be instantiated	
+            // unless	the	object	contains	a	public	factory	method	that	can	be	called to	create	instances	of	the	class. 
+        }
+
+        // Constructors	can	be	overloaded,	so	an	object	can	contain	multiple	versions	of a constructor with different   signatures
+        public void OverloadedConstructors()
+        {
+            AlienClassConstr x = new AlienClassConstr(100, 100, 4);
         }
 
     }
