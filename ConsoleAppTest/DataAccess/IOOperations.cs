bd2ebd7b6e488c63f125abd345f869d5588425fc
maxvonlancaster <windchange7@gmail.com>
2019-10-30 14:32:42 +0200

    using a filestream

--- ConsoleAppTest/DataAccess/IOOperations.cs
+++ ConsoleAppTest/DataAccess/IOOperations.cs
@@ -1,10 +1,142 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ConsoleAppTest.DataAccess
 {
+    // File access is a very slow activity when compared with the speed of modern processors, so we are also going to investigate the use of
+    // asynchronous i/o, which can be used to keep an application responsive even when it is reading or writing large amounts of data.
     public class IOOperations
     {
+        // A stream is a software object that represents a stream of data. The .NET
+        // framework provides a Stream class that serves as the parent type for a range of classes that can be used to read and write data.
+        // Write, read and position the "file pointer".
+
+        // The FileStream object provides a stream instance connected to a file. The stream object instance converts calls into the stream into commands for the
+        // filesystem on the computer running the program.The file system provides the interface to the physical device performing the data storage for the computer
+        // c# app <-> stream object <-> Filestream <-> storage device
+        public void UsingAFileStrem()
+        {
+            // Writing to a file
+            FileStream outputStream = new FileStream("OutputText.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            string outPutMessageString = "Hello world";
+            byte[] outputMessageBytes = Encoding.UTF8.GetBytes(outPutMessageString);
+            outputStream.Write(outputMessageBytes);
+            outputStream.Close();
+
+            FileStream inputStream = new FileStream("OutputText.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            long fileLength = inputStream.Length;
+            byte[] readBytes = new byte[fileLength];
+            inputStream.Read(readBytes, 0, (int)fileLength);
+            string readString = Encoding.UTF8.GetString(readBytes);
+            inputStream.Close();
+            Console.WriteLine("Text: {0}", readString);
+
+            // FileMode: Append, Create, CreateNew(exception thrown if already exists), Open, OpenOrCreate, Truncate(open for writing and remove any existibng content)
+            // FileAccess: Read, ReadWrite, Write
+        }
+
+        // 
+        public void FileStremAndIDisposable()
+        {
+
+        }
+
+        // 
+        public void StreamWriterAndReader()
+        {
+
+        }
+
+        // 
+        public void StoringCompressedFiles()
+        {
+
+        }
+
+        // 
+        public void UseFileClass()
+        {
+
+        }
+
+        // 
+        public void StreamExceptions()
+        {
+
+        }
+
+        // 
+        public void DriveInformation()
+        {
+
+        }
+
+        // 
+        public void UsingFileInfo()
+        {
+
+        }
+
+        // 
+        public void TheDirectoryClass()
+        {
+
+        }
+
+        // 
+        public void TheDirectoryClassInfo()
+        {
+
+        }
+
+        // 
+        public void UsingPath()
+        {
+
+        }
+
+        // 
+        public void CSharpPorgrams()
+        {
+
+        }
+
+        // 
+        public void HttpWebRequest()
+        {
+
+        }
+
+        //
+        public void WebClient()
+        {
+
+        }
+
+        // 
+        public void WebClientAsync()
+        {
+
+        }
+
+        // 
+        public void HttpClient()
+        {
+
+        }
+
+        // 
+        public void AsynchronousFileWriting()
+        {
+
+        }
+
+        // 
+        public void FileExceptions()
+        {
+
+        }
     }
 }
--- ConsoleAppTest/DebugAndSecurity/ValidateInput.cs
+++ ConsoleAppTest/DebugAndSecurity/ValidateInput.cs
@@ -7,15 +7,16 @@ using System.Xml.Serialization;
 
 namespace ConsoleAppTest.DebugAndSecurity
 {
-    // The	first	step	in	creating	a	secure	application	is	ensuring	that	incoming	data	is valid.	Data	in	your	applications	
-    // must	come	from	somewhere;	whether	it	is	items entered	into	the	application	user	interface,	or	packets	of	formatted	values received
-    // from	another	application.	In	every	case	you	need	to	make	sure	that	your application	is	resilient	in	its	response
-    // to	invalid	data.	You	also	need	to	keep	in mind	that	invalid	data	may	have	been	entered	with	malicious	intent.	
-    // It	is important	that	a	poorly	thought	out	response	to	invalid	data	doesn’t	expose	your systems	to	attack. 
-    // A	JSON	document	contains	a	number	of	name/value	pairs	that	represent	the	data in	an	application.	A	JSON	document	
-    // can	also	contain	arrays	of	JSON	objects. JSON	documents	map	very	well	onto	objects	in	an	object-oriented	language, although	
-    // JSON	itself	is	not	tied	to	any	one	programming	language.	JSON	is therefore	very	useful	if	you	want	to	share	data	
-    // between	programs	written	in different	languages. 
+    // The first step in creating a secure application is ensuring that incoming data is valid. Data in your applications	
+    // must come from somewhere; whether it is items entered into the application user interface, or packets of formatted values received
+    // from another application. In every case you need to make sure that your application is resilient in its response
+    // to invalid data. You also need to keep in mind that invalid data may have been entered with malicious intent.	
+    // It is important that a poorly thought out response to invalid data doesn’t expose your systems to attack. 
+
+    // A JSON document contains a number of name/value pairs that represent the data in an application. A JSON document	
+    // can also contain arrays of JSON objects. JSON documents map very well onto objects in an object-oriented language, although	
+    // JSON itself is not tied to any one programming language.	JSON is therefore very useful if you want to share data	
+    // between programs written in different languages. 
     public class ValidateInput
     {
         // When you start to use JSON in your programs there are a few things that you need to be aware of from a security and class design point of view:
