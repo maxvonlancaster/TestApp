28e6bfce76a6467ca62a3759dd548e2dc4785d8b
windchange7 <windchange7@gmail.com>
2019-10-30 09:55:03 +0200

    chapter 3 started

--- ConsoleAppTest/DebugAndSecurity/ValidateInput.cs
+++ ConsoleAppTest/DebugAndSecurity/ValidateInput.cs
@@ -4,7 +4,41 @@ using System.Text;
 
 namespace ConsoleAppTest.DebugAndSecurity
 {
+    // The	first	step	in	creating	a	secure	application	is	ensuring	that	incoming	data	is valid.	Data	in	your	applications	
+    // must	come	from	somewhere;	whether	it	is	items entered	into	the	application	user	interface,	or	packets	of	formatted	values received
+    // from	another	application.	In	every	case	you	need	to	make	sure	that	your application	is	resilient	in	its	response
+    // to	invalid	data.	You	also	need	to	keep	in mind	that	invalid	data	may	have	been	entered	with	malicious	intent.	
+    // It	is important	that	a	poorly	thought	out	response	to	invalid	data	doesn’t	expose	your systems	to	attack. 
+    // A	JSON	document	contains	a	number	of	name/value	pairs	that	represent	the	data in	an	application.	A	JSON	document	
+    // can	also	contain	arrays	of	JSON	objects. JSON	documents	map	very	well	onto	objects	in	an	object-oriented	language, although	
+    // JSON	itself	is	not	tied	to	any	one	programming	language.	JSON	is therefore	very	useful	if	you	want	to	share	data	
+    // between	programs	written	in different	languages. 
     public class ValidateInput
     {
+        // 
+        public void CreatingJson()
+        {
+
+        }
+    }
+
+    public class MusicTrack
+    {
+        public MusicTrack(string artist, string title, int length)
+        {
+            Artist = artist;
+            Title = title;
+            Length = length;
+        }
+
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public int Length { get; set; }
+
+        //	ToString	that	overrides	the	behavior	in	the	base	class				
+        public	override	string	ToString()
+        {
+            return	Artist + " " + Title + " " + Length.ToString() + "seconds long" ;
+        }
     }
 }
--- RandomAvarages/MainWindow.xaml.cs
+++ RandomAvarages/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using ConsoleAppTest;
 
 namespace RandomAvarages
 {
@@ -20,9 +21,35 @@ namespace RandomAvarages
     /// </summary>
     public partial class MainWindow : Window
     {
+        // A	Universal	Application	(Windows	Store)	Windows	Presentation	Foundation (WPF)	application	or	a	WinForms	application	can	be	
+        // regarded	as	having	a	single thread of  execution that	is	dealing with    the user    interface.	At any given instant this	thread will   
+        // be performing  a particular  action.In other   words,	when code	in an event handler is	running	in	response to a particular  event,	
+        // such	as	a button press,	it	is	not possible	for	code	in	any other part of the user    interface to  execute.You sometimes   see this   
+        // behavior    in badly written applications, where the user interface of  the application becomes unresponsive	while	an action	is	carried	out.
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private double ComputeTheAverages(double input)
+        {
+            double total = 0;
+            Random random = new Random();
+            for (long values = 0; values < input; values++)
+            {
+                total += random.NextDouble();
+            }
+            return total / input;
+        }
+
+        private static void ButtonClicked(object sender, RoutedEventArgs args)
+        {
+            MainWindow mw = new MainWindow();
+            mw.nameInput.Text = (String)((Button)sender).Tag;
+            ((MainWindow)Application.Current.MainWindow).nameInput.Text = (String)((Button)sender).Tag;
+            long input = long.Parse(mw.nameInput.Text);
+        }
+
+
     }
 }
