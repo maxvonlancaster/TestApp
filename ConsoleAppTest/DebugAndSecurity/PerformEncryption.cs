00a28835e9800084cf799d1ec5fa9a6cc333d69f
windchange7 <windchange7@gmail.com>
2019-11-11 09:27:29 +0200

    rsa encryption

--- ConsoleAppTest/DebugAndSecurity/PerformEncryption.cs
+++ ConsoleAppTest/DebugAndSecurity/PerformEncryption.cs
@@ -61,7 +61,7 @@ namespace ConsoleAppTest.DebugAndSecurity
                         new CryptoStream(encryptMemoryStream, encryptor, CryptoStreamMode.Write))
                     {
                         // make a stream writer from the cryptostream
-                        using (StreamWriter swEncrypt = 
+                        using (StreamWriter swEncrypt =
                             new StreamWriter(encryptCryptoStream))
                         {
                             //Write the secret message to the stream.
@@ -127,10 +127,51 @@ namespace ConsoleAppTest.DebugAndSecurity
             Console.WriteLine("Decrypted text: ", decryptedText);
         }
 
-        // 
+        // The encryption process in .NET is an example of good class design. All of the encryption classes, including Aes, are extensions of the base class
+        // SymmetricAlgorithm, which is in the  System.Security.Cryptography namespace.
+
+        // RSA (Rivest–Shamir–Adleman) is a very popular asymmetric encryption        standard.The RSACryptoServiceProvider class in the
+        // System.Security.Cryptography namespace will perform encryption        and decryption of data using this standard.
         public void RsaEncryption()
         {
-
+            string plainText = "This is my super secret data";
+            Console.WriteLine("Plain text: {0}", plainText);
+            // RSA works on byte arrays, not strings of text
+            // This will convert our input string into bytes and back
+            ASCIIEncoding converter = new ASCIIEncoding();
+            // Convert the plain text into a byte array
+            byte[] plainBytes = converter.GetBytes(plainText);
+            DumpBytes("Plain bytes: ", plainBytes);
+            byte[] encryptedBytes;
+            byte[] decryptedBytes;
+            // Create a new RSA to encrypt the data
+            // should be wrapped in using for production code
+            RSACryptoServiceProvider rsaEncrypt = new RSACryptoServiceProvider();
+            // get the keys out of the encryptor
+            string publicKey = rsaEncrypt.ToXmlString(includePrivateParameters: false);
+            Console.WriteLine("Public key: {0}", publicKey);
+            string privateKey = rsaEncrypt.ToXmlString(includePrivateParameters: true);
+            Console.WriteLine("Private key: {0}", privateKey);
+            // Now tell the encyryptor to use the public key to encrypt the data
+            rsaEncrypt.FromXmlString(privateKey);
+            // Use the encryptor to encrypt the data. The fOAEP parameter
+            // specifies how the output is "padded" with extra bytes
+            // For maximum compatibility with receiving systems, set this as
+            // false
+            encryptedBytes = rsaEncrypt.Encrypt(plainBytes, fOAEP: false);
+            DumpBytes("Encrypted bytes: ", encryptedBytes);
+            // Now do the decode - use the private key for this
+            // We have sent someone our public key and they
+            // have used this to encrypt data that they are sending to us
+            // Create a new RSA to decrypt the data
+            // should be wrapped in using for production code
+            RSACryptoServiceProvider rsaDecrypt = new RSACryptoServiceProvider();
+            // Configure the decryptor from the XML in the private key
+            rsaDecrypt.FromXmlString(privateKey);
+            decryptedBytes = rsaDecrypt.Decrypt(encryptedBytes, fOAEP: false);
+            DumpBytes("Decrypted bytes: ", decryptedBytes);
+            Console.WriteLine("Decrypted string: {0}",
+            converter.GetString(decryptedBytes));
         }
 
         // 
