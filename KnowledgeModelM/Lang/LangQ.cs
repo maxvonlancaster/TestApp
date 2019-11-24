d7862c1ac6fe87ad2b030a0d7e752c40497ad300
windchange7 <windchange7@gmail.com>
2019-11-24 19:34:36 +0200

    box model

--- KnowledgeModelM/Lang/LangQ.cs
+++ KnowledgeModelM/Lang/LangQ.cs
@@ -9,6 +9,12 @@ namespace KnowledgeModel.Lang
 {
     public class LangQ
     {
+        // Properties and automatic properties
+        public int Id { get; set; } // automatic property -> no additional logic of access
+        public string Name { get; set; } = "Name"; // in C#6 and later -> automatic prop. can be initialized
+        // 
+
+
         // Static using -> allows all the accessible static members of a type to be imported, making them available without qualification in subseq. code.
         // great if you have a set of functions related to certain domain
         public void StaticUsing()
