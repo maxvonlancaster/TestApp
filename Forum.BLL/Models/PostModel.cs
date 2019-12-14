43808efee50fb604d2ad5d3814096b6a0f6b1262
windchange7 <windchange7@gmail.com>
2019-12-14 18:02:22 +0200

    initial controller generated

--- Forum.BLL/Models/PostModel.cs
+++ Forum.BLL/Models/PostModel.cs
@@ -6,5 +6,12 @@ namespace Forum.BLL.Models
 {
     public class PostModel
     {
+        public int PostId { get; set; }
+        public byte[] Media { get; set; }
+        public string Text { get; set; }
+        public int Rating { get; set; }
+        public string Poster { get; set; }
+        public string SubForum { get; set; }
+        public string Category { get; set; }
     }
 }
