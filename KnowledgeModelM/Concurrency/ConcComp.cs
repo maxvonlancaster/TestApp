4d65b6e92d41a0072fd1bf2efe47db27e819c623
windchange7 <windchange7@gmail.com>
2019-12-29 22:53:13 +0200

    sometext

--- KnowledgeModelM/Concurrency/ConcComp.cs
+++ KnowledgeModelM/Concurrency/ConcComp.cs
@@ -11,6 +11,13 @@ namespace KnowledgeModel.Concurrency
         // 1.1 Introduction to concurrency 
 
         // Concurrency -> doing more than one thing at a time
+        // Multithreading  -> A form of concurrency that uses multiple threads of execution
+        // Parallel Processing -> Doing lots of work by dividing it up among multiple threads that run concurrently
+        // Asynchronous Programming -> A form of concurrency that uses futures or callbacks to avoid unnecessary threads.
+
+        // A future (or promise) is a type that represents some operation that will complete in the future.The modern future types in 
+        //.NET are Task and Task<TResult>.Older asynchronous APIs use callbacks or events instead of futures.Asynchronous programming
+        // is centered around the idea of an asynchronous operation: some operation that is started that will complete some time later.W
 
         // 1.2 Introduction to Asynchronous Programming 
         public async Task DoSomethingAsync()
@@ -27,6 +34,12 @@ namespace KnowledgeModel.Concurrency
             await Task.Delay(TimeSpan.FromSeconds(1));
 
             Trace.WriteLine(val);
+
+            // An async method begins executing synchronously, just like any other method. Within  an async method, the await keyword performs 
+            // an asynchronous wait on its argument. First, it checks whether the operation is already complete; if it is, it continues executing
+            //(synchronously).Otherwise, it will pause the async method and return an incomplete task.When that operation completes some time later, 
+            // the async method will resume executing.
+
         }
 
         public async Task DoSomethingNewAsync()
