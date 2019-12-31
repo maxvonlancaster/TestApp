c776c81b69d0c432114ab310dbb3f114b6b5ab1c
maxvonlancaster <windchange7@gmail.com>
2019-12-31 12:17:20 +0200

    DelayResult

--- KnowledgeModelM/Concurrency/AsyncBasics.cs
+++ KnowledgeModelM/Concurrency/AsyncBasics.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace KnowledgeModel.Concurrency
 {
     public class AsyncBasics
     {
         // Pausing for a Period of Time (chapter 2.1)
-        // 
+        // The Task type has a static method Delay that returns a task that completes after the specified time.
+        static async Task<T> DelayResult<T>(T result, TimeSpan delay) 
+        {
+            await Task.Delay(delay);
+            return result;
+        }
     }
 }
