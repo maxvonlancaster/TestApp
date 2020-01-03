8c258a09635077041b26ec28cd8c791088e42685
maxvonlancaster <windchange7@gmail.com>
2020-01-03 14:33:45 +0200

    WaitingAny

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -6,6 +6,8 @@ using ConsoleAppTest.DataAccess;
 using ConsoleAppTest.Services;
 using ConsoleAppTest.Patterns;
 using KnowledgeModel.Lang;
+using System.Threading.Tasks;
+using KnowledgeModel.Concurrency;
 
 namespace ConsoleAppTest
 {
@@ -15,7 +17,14 @@ namespace ConsoleAppTest
         {
             var service = new TestService();
             service.TestMethod();
+            AsyncMethods().Wait();
             //Console.WriteLine(CalcService.Factorial(20));
+        }
+
+        static async Task AsyncMethods() 
+        {
+            var service = new AsyncBasics();
+            await service.WaitingAny();
         }
     }
 }
--- KnowledgeModelM/Concurrency/AsyncBasics.cs
+++ KnowledgeModelM/Concurrency/AsyncBasics.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -44,9 +47,9 @@ namespace KnowledgeModel.Concurrency
         // Waiting for a Set of Tasks to Complete (chapter 2.4)
         // The framework provides a Task.WhenAll method for this purpose. This method takes several tasks and returns a task 
         // that completes when all of those tasks have completed
-        static async Task WaitingSet() 
+        static async Task WaitingSet()
         {
-            Task task1 = Task.Run(() => 
+            Task task1 = Task.Run(() =>
             {
                 Thread.Sleep(1000);
                 Console.WriteLine("t1");
@@ -65,16 +68,78 @@ namespace KnowledgeModel.Concurrency
             await t;
         }
 
+
+
         // Waiting for Any Task to Complete(chapter 2.5)
+        // For example, you could request stock quotes from multiple web services simultaneously, but you only care about the 
+        // first one that responds.
+        // Use the Task.WhenAny method. This method takes a sequence of tasks and returns a task that completes when any of the 
+        // tasks complete.The result of the returned task is the task that completed
+        public async Task WaitingAny()
+        {
+            string[] urls =
+            {
+                "https://www.google.com.ua/",
+                "https://www.bing.com/"
+            };
+
+            var httpClient = new HttpClient();
+            Task<string> task1 = httpClient.GetStringAsync(urls[0]);
+            Task<string> task2 = httpClient.GetStringAsync(urls[1]);
+
+            Task<string> completedTask = await Task.WhenAny(task1, task2);
+            string data = await completedTask;
+            Console.WriteLine(data);
+        }
+        // The task returned by Task.WhenAny never completes in a faulted or canceled state. It always results in the first Task to complete; 
+        // if that task completed with an exception, then the exception is not propogated to the task returned by Task.WhenAny.For this
+        // reason, you should usually await the task after it has completed
+        // When the first task completes, consider whether to cancel the remaining tasks. If the other tasks are not canceled but are 
+        // also never awaited, then they are abandoned.Aban‐doned tasks will run to completion, and their results will be ignored.Any exceptions
+        // from those abandoned tasks will also be ignored.
+
 
 
         // Processing Tasks as They Complete(chapter 2.6)
+        // You have a collection of tasks to await, and you want to do some processing on each task after it completes.However, you want 
+        // to do the processing for each one as soon as it completes, not waiting for any of the other tasks.
+        static async Task<int> DelayAndReturnAsync(int val)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(val));
+            return val;
+        }
+
+        static async Task AwaitAndProcessAsync(Task<int> task)
+        {
+            var result = await task;
+            Trace.WriteLine(result);
+        }
+
+        // This method now prints "1", "2", and "3".
+        public async Task ProcessTasksAsync()
+        {
+            // Create a sequence of tasks.
+            Task<int> taskA = DelayAndReturnAsync(2);
+            Task<int> taskB = DelayAndReturnAsync(3);
+            Task<int> taskC = DelayAndReturnAsync(1);
+            var tasks = new[] { taskA, taskB, taskC };
+            var processingTasks = (from t in tasks
+                                   select AwaitAndProcessAsync(t)).ToArray();
+            // Await all processing to complete
+            await Task.WhenAll(processingTasks);
+        }
+
+
 
 
         // Avoiding Context for Continuations(chapter 2.7)
 
 
+
+
         // Handling Exceptions from async Task Methods(chapter 2.8)
+
+
 
 
         // Handling Exceptions from async Void Methods(chapter 2.9)
