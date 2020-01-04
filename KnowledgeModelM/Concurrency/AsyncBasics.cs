c5a0b61e341bef32d5a1f69c5d80807e12f4177d
windchange7 <windchange7@gmail.com>
2020-01-04 20:09:23 +0200

    configure await

--- KnowledgeModelM/Concurrency/AsyncBasics.cs
+++ KnowledgeModelM/Concurrency/AsyncBasics.cs
@@ -133,8 +133,20 @@ namespace KnowledgeModel.Concurrency
 
 
         // Avoiding Context for Continuations(chapter 2.7)
-
-
+        // When an async method resumes after an await, by default it will resume executing
+        // within the same context.This can cause performance problems if that context was a UI
+        // context and a large number of async methods are resuming on the UI context.
+        // To avoid resuming on a context, await the result of ConfigureAwait and pass false for its continueOnCapturedContext parameter:
+        async Task ResumeOnContextAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            // This method resumes within the same context.
+        }
+        async Task ResumeWithoutContextAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            // This method discards its context when it resumes.
+        }
 
 
         // Handling Exceptions from async Task Methods(chapter 2.8)
