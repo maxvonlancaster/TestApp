077eade60b211564c1765ff9068274a0d5727f75
windchange7 <windchange7@gmail.com>
2020-01-11 22:14:47 +0200

    custom attributes

--- KnowledgeModelM/Lang/LangComp.cs
+++ KnowledgeModelM/Lang/LangComp.cs
@@ -59,8 +59,37 @@ namespace KnowledgeModel.Lang
 
         public void CustomAttributes()
         {
+            // Attributes are metadata extensions that give additional information to the compiler about the elements in the program code at 
+            // runtime.Attributes are used to impose conditions or to increase the efficiency of a piece of code. There are built -in 
+            // attributes present in C# but programmers may create their own attributes, such attributes are called Custom attributes
 
+            // You can create your own custom attributes by defining an attribute class, a class that derives 
+            // directly or indirectly from Attribute, which makes identifying attribute definitions in metadata fast and easy.
+            CustomMethod();
         }
+
+        [System.AttributeUsage(System.AttributeTargets.Class |
+                       System.AttributeTargets.Struct | AttributeTargets.Method)] // Determines how a custom attribute class can be used. AttributeUsageAttribute is an attribute you apply to custom attribute definitions. 
+        public class Author : System.Attribute
+        {
+            private string name;
+            public double version;
+
+            public Author(string name)
+            {
+                this.name = name;
+                version = 1.0;
+            }
+            // Any public read-write fields or properties are named parameters. 
+        }
+
+        [Author("Batman", version = 1.5)]
+        public static void CustomMethod() 
+        {
+        
+        }
+
+
 
         public void DisposeAndFinalizePatterns()
         {
