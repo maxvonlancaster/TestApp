3e8aca65116e6d6bde0acae8848c28bd57b98635
windchange7 <windchange7@gmail.com>
2020-03-05 00:06:47 +0200

    paths

--- MLPlayground/Services/TestService.cs
+++ MLPlayground/Services/TestService.cs
@@ -10,18 +10,18 @@ namespace MLPlayground.Services
     {
         private readonly MLContext _context;
 
-        private string _inputDataPath;
+        private string[] _inputDataPaths;
 
         public TestService()
         {
             _context = new MLContext();
-            _inputDataPath = "..//DataSets//COTW//countries of the world.csv";
+            _inputDataPaths[0] = "..//DataSets//COTW//countries of the world.csv";
         }
 
         public void GetTrainAndTestData() 
         {
             // load from csv
-            var dataView = _context.Data.LoadFromTextFile<Country>(_inputDataPath, hasHeader: true, separatorChar: ',');
+            var dataView = _context.Data.LoadFromTextFile<Country>(_inputDataPaths[0], hasHeader: true, separatorChar: ',');
             // Split the data randomly, in 90:10 ratio, for training and evaluation data respectively.
             var data = _context.Data.TrainTestSplit(dataView, testFraction: 0.1);
         }
