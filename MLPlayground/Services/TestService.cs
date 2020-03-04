97417292b1f0e0b6a7d6e89a55cf0021dfe230a0
windchange7 <windchange7@gmail.com>
2020-03-05 00:06:08 +0200

    data split

--- MLPlayground/Services/TestService.cs
+++ MLPlayground/Services/TestService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.ML;
+using MLPlayground.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,13 +8,22 @@ namespace MLPlayground.Services
 {
     public class TestService
     {
-        private readonly MLContext _mLContext;
+        private readonly MLContext _context;
 
         private string _inputDataPath;
 
         public TestService()
         {
-            _mLContext = new MLContext();
+            _context = new MLContext();
+            _inputDataPath = "..//DataSets//COTW//countries of the world.csv";
+        }
+
+        public void GetTrainAndTestData() 
+        {
+            // load from csv
+            var dataView = _context.Data.LoadFromTextFile<Country>(_inputDataPath, hasHeader: true, separatorChar: ',');
+            // Split the data randomly, in 90:10 ratio, for training and evaluation data respectively.
+            var data = _context.Data.TrainTestSplit(dataView, testFraction: 0.1);
         }
 
 
