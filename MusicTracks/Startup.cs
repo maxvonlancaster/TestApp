d12a2d0bb6330c64a12f3b11776878861c248c0a
maxvonlancaster <windchange7@gmail.com>
2020-03-06 10:20:57 +0200

    final fix of asp net proj

--- MusicTracks/Startup.cs
+++ MusicTracks/Startup.cs
@@ -70,13 +70,12 @@ namespace MusicTracks
             app.UseStaticFiles();
             app.UseCookiePolicy();
             app.UseAuthentication();
+            app.UseRouting();
             //app.UseIdentity();
 
-            app.UseMvc(routes =>
+            app.UseEndpoints(routes =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapControllerRoute("default", "{controller=Home}/{action=Index}");
             });
         }
     }
