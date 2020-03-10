4f14e5fbf33917844208b297f759658e819740b0
windchange7 <windchange7@gmail.com>
2020-03-10 08:54:23 +0200

    Promise

--- MusicTracks/wwwroot/js/JsFundamentalsC.js
+++ MusicTracks/wwwroot/js/JsFundamentalsC.js
@@ -73,11 +73,35 @@
     }
 
     Clojures() {
+        // JavaScript variables can belong to the local or global scope. Global variables can be made local(private) with closures.
+        // A local variable can only be used inside the function where it is defined. It is hidden from other functions and other scripting code.
+
+        var add = (function () {
+            var counter = 0;
+            return function () { counter += 1; return counter }
+        })();
+
+        add();
+        add();
+        add();
+
+        // The variable add is assigned the return value of a self - invoking function.
+        // The self - invoking function only runs once.It sets the counter to zero(0), and returns a function expression.
+        // This way add becomes a function.The "wonderful" part is that it can access the counter in the parent scope.
+        // This is called a JavaScript closure.It makes it possible for a function to have "private" variables.
+        // The counter is protected by the scope of the anonymous function, and can only be changed using the add function.
 
     }
 
     AjaxJson() {
+        // AJAX = Asynchronous JavaScript And XML.
+        // AJAX is not a programming language.
+        // AJAX just uses a combination of:
+        // A browser built -in XMLHttpRequest object(to request data from a web server)
+        // JavaScript and HTML DOM(to display or use the data)
 
+        // AJAX allows web pages to be updated asynchronously by exchanging data with a web server behind the scenes. This means that it is 
+        // possible to update parts of a web page, without reloading the whole page.
     }
 
     Es6() {
@@ -163,7 +187,12 @@
     }
 
     Promise() {
+        // The Promise object represents the eventual completion (or failure) of an asynchronous operation, and its resulting value.
 
+        // A Promise is in one of these states:
+        // pending: initial state, neither fulfilled nor rejected.
+        // fulfilled: meaning that the operation completed successfully.
+        // rejected: meaning that the operation failed.
     }
 
 }
